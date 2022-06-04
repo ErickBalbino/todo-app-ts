e9b5b257cb1602165d040bb32ee72cd390b40b98
Erick <erickbaddog2@gmail.com>
2022-06-04 01:45:48 -0300

    started structure the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Todos from './components/Todos'
+
+const App =() => {
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <ul>
+        <li>Learn React</li>
+        <li>Learn typescript</li>
+    </ul>
+  )
+}
+
+export default Todos
